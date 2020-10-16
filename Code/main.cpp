cf40c299c4cecd022e11f5efd5b0dec2b2b79c7c
romansko90 <romansko90@gmail.com>
2020-10-16 18:57:40 +0300

    Add test logic within main file

--- Code/main.cpp
+++ Code/main.cpp
@@ -2,19 +2,77 @@
 //
 
 #include "CCSVParser.h"
+#include <fstream>
+
+struct SampleStruct
+{
+	int32_t     intVal;
+	float       floatVal;
+	const char* stringVal;
+};
+
+enum
+{
+	INT_VAL,
+	FLOAT_VAL,
+	STR_VAL,
+	ELEMENTS_NUM
+};
+
+SampleStruct* makeStructTable(CCSVParser& parser)
+{
+	SampleStruct* stb = nullptr;
+	if (parser.getColumnsSize() == ELEMENTS_NUM)
+	{
+		const int rowSize = parser.getRowSize();
+		stb = new SampleStruct[rowSize];
+		for (auto i = 0; i < rowSize; ++i)
+		{
+			parser.getValue(i, INT_VAL,   stb[i].intVal);
+			parser.getValue(i, FLOAT_VAL, stb[i].floatVal);
+			parser.getValue(i, STR_VAL,    &(stb[i].stringVal));
+		}
+	}
+	return stb;
+}
 
 int main()
 {
 	CCSVParser parser;
+	SampleStruct* stb = nullptr;
 	if (parser.parse(".//TestData//input.csv"))  // path to example data.
 	{
 		table_t tb = parser.getTable();
 
 		row_t row;
 		row.push_back("Strings");
+		row.push_back("Integer Values");
 		row.push_back("Float Values");
 		parser.filter(row);
 		parser.log();
+
+		stb = makeStructTable(parser);
+	}
+
+	if (stb != nullptr)
+	{
+		std::ofstream output;
+		output.open(".//TestData//output.csv");
+		const int32_t headerSize = parser.getHeader().size();
+		auto hdr = parser.getHeader();
+		auto title = hdr.begin();
+		for (; title != hdr.end()-1; ++title)
+		{
+			output << *title << ",";
+		}
+		output << *title << std::endl;
+
+		for (auto i = 0; i < parser.getRowSize(); ++i)
+		{
+			output << stb[i].intVal << "," << stb[i].floatVal << "," << stb[i].stringVal << std::endl;
+		}
+		output.close();
+		delete[] stb;
 	}
  
 	return 0;
